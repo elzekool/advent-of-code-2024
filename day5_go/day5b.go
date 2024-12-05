7847635232ed25d94aaef6aadd0288e915294afd
Elze Kool <e.kool@youweagency.com>
2024-12-05 15:07:34 +0100

    Day 5, trying go

---
+++ day5_go/day5b.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"slices"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	input, _ := os.ReadFile("input.txt")
+	blocks := strings.Split(string(input), "\n\n")
+
+	sorts := make(map[string]int)
+	for _, sortItem := range strings.Split(blocks[0], "\n") {
+		nrs := strings.Split(sortItem, "|")
+		sorts[nrs[0]+"|"+nrs[1]] = -1
+		sorts[nrs[1]+"|"+nrs[0]] = 1
+	}
+
+	sum := 0
+	for _, line := range strings.Split(blocks[1], "\n") {
+		pages := strings.Split(line, ",")
+		slices.SortFunc(pages, func(a, b string) int {
+			if val, ok := sorts[a+"|"+b]; ok {
+				return val
+			}
+			return 0
+		})
+
+		sortedPages := strings.Join(pages, ",")
+		if line == sortedPages {
+			continue
+		}
+
+		newval, _ := strconv.Atoi(pages[len(pages)/2])
+		sum += newval
+	}
+
+	fmt.Println(sum)
+}
